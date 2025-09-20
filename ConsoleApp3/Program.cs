da2f6388867cdaa7695af307c3de617425b99134
ManInSpace <klimentmart@gmail.com>
2025-09-20 14:57:37 +0300

    Initial commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        int[] nominals = { 5000, 2000, 1000, 500, 200, 100 };
+        while (true) {
+            Console.WriteLine("Введите нужную сумму (не более 150.000 и кратную 100)");
+            int input = int.Parse(Console.ReadLine());
+            if (input % 100 == 0 && input <= 150000)
+            {
+                foreach (var nominal in nominals)
+                {
+                    int sum = input / nominal;
+                    if (sum > 0)
+                    {
+                        Console.WriteLine($"{nominal}Р: {sum} куп.");
+                    }
+                    input %= nominal;
+
+                }
+            }
+            
+        }
+    }
+}
